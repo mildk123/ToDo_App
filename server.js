059517b2b8c78ce26d54fc69a6616b91b41b6fb2
Milad Khan <mildk123@gmail.com>
2019-01-21 04:47:26 +0500

    fixed

--- server.js
+++ server.js
@@ -31,23 +31,25 @@ app.get('/todos/getAll', (req, res) => {
 app.use(express.static(path.join(__dirname, '/client/build')));
 
 //production mode
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
+// if (process.env.NODE_ENV === 'production') {
+  // app.use(express.static(path.join(__dirname, '/client/build')));
   //
   app.get('*', (req, res) => {
     res.sendfile(path.join(__dirname = '/client/build/index.html'));
   })
-}
+// }
 
+
+
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+})
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
-//build mode
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/public/index.html'));
-})
 
 app.use(cors());
 app.use(express.json());
