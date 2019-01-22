69a7c25ce62b369a4d3bd99c162cca9dd81d4c6f
Milad Khan <mildk123@gmail.com>
2019-01-22 16:57:54 +0500

    moved to folder

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, '/client/build')));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, 'client/build/index.html'));
+    response.sendFile(path.join(__dirname, '/client/build/index.html'));
   });
 }
 
