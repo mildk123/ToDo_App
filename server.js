8732d21ae3427753b04db243f0e0b6d475d95719
Milad Khan <mildk123@gmail.com>
2019-01-22 16:56:34 +0500

    moved to folder

--- server.js
+++ server.js
@@ -17,26 +17,25 @@ app.use(cors());
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-    console.log("db connected!")
+  console.log("db connected!")
 });
 
 
-app.set( 'port', ( port || 5000 ));
-app.listen( app.get( 'port' ), () => {
-  console.log( 'Node server is running on port ' + app.get( 'port' ));
-  })
+app.set('port', (port || 5000));
+app.listen(app.get('port'), () => {
+  console.log('Node server is running on port ' + app.get('port'));
+})
 
 
 //Static file declaration
 app.use(express.static(path.join(__dirname, '/client/build')));
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
+  app.use(express.static('client/build'));
+  app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'client/build/index.html'));
+  });
 }
-
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build'));
-});
 
 ///////////////// APIs ////////////////////
 // Get all todos
@@ -53,29 +52,29 @@ app.get('/todos/getAll', (req, res) => {
 // Insert ToDo
 app.post("/todos/add", (req, res) => {
   console.log('Pushing Todo');
-  
+
   const todo = new Todos(req.body);
 
   todo.save()
-  .then(() => res.send({message: "Todo inserted successfully!"}))
-  .catch(e => res.send({message: e.message}))
+    .then(() => res.send({ message: "Todo inserted successfully!" }))
+    .catch(e => res.send({ message: e.message }))
 })
 
 // Update ToDo
 app.put("/todos/update", (req, res) => {
   console.log('Updating Todo');
 
-  Todos.updateOne({email: req.body.email}, {age: req.body.age})
-  .then(result => res.send(result))
-  .catch(e => res.send({message: e.message}))
+  Todos.updateOne({ email: req.body.email }, { age: req.body.age })
+    .then(result => res.send(result))
+    .catch(e => res.send({ message: e.message }))
 })
 
 // Remove ToDo
 app.delete("/todos/remove", (req, res) => {
   console.log('removing Todo');
-  Todos.deleteOne({title: req.body.title})
-  .then(result => res.send(result))
-  .catch(e => res.send({message: e.message}))
+  Todos.deleteOne({ title: req.body.title })
+    .then(result => res.send(result))
+    .catch(e => res.send({ message: e.message }))
 })
 
 
