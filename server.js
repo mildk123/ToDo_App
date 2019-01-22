058d03bbd311cce993d6546a069ef51877235d39
Milad Khan <mildk123@gmail.com>
2019-01-22 15:52:10 +0500

    removed

--- server.js
+++ server.js
@@ -1,111 +1,18 @@
 
-
 const express = require('express');
-const cors = require('cors')
+const Todos = require('./model/Todos')
+
 const app = express();
-const Todos = require('./server/model/Todos')
-const mongoose = require('./server/config/db');
+
+const cors = require('cors')
+const mongoose = require('./config/db');
 
 const path = require('path');
 var port = process.env.PORT || 5000;
 
-// var createError = require('http-errors');
+app.use(express.json());
+app.use(cors());
 
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-
-// var app = express();
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
-
-// error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-
-
-
-app.listen(5000, function () {
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
-
-
-
-// // get all todos
-// app.get('/todos/getAll', (req, res) => {
-//   console.log('requesting todos')
-//   // res.json(["Tony", "Lisa", "Michael", "Ginger"]);
-
-//   Todos.find({})
-//     .then(result => res.send(result))
-//     .catch(e => res.send({ message: e.message }))
-
-// });
-
-// //Static file declaration
-app.use(express.static(path.join(__dirname, '/client/build')));
-
-// //production mode
-// // if (process.env.NODE_ENV === 'production') {
-// // app.use(express.static(path.join(__dirname, '/client/build')));
-// //
-app.get('*', (req, res) => {
-  res.sendfile(path.join(__dirname = '/client/build/index.html'));
-})
-// // }
-
-
-// //build mode
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-})
-
-
-
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use('/', require('./server/routes/index'))
-
-
-
-
-// module.exports = app;
-
-
-const express = require('express');
-const app = express();
-const mongoose = require('./server/config/db');
-// const cors = require('cors')
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -113,12 +20,56 @@ db.once('open', function() {
     console.log("db connected!")
 });
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log("Hamara server shareef is working!")
+app.listen(port || 5000, () => {
+  console.log("Hamara server shareef is working!", port)
 })
 
-// app.use(cors());
 
-app.use(express.json());
+//Static file declaration
+app.use(express.static(path.join(__dirname, '/client/build')));
 
-app.use('/', require('./server/routes/index'))
+
+///////////////// APIs ////////////////////
+// Get all todos
+app.get('/todos/getAll', (req, res) => {
+  console.log('requesting todos')
+  // res.json(["Tony", "Lisa", "Michael", "Ginger"]);
+
+  Todos.find({})
+    .then(result => res.send(result))
+    .catch(e => res.send({ message: e.message }))
+
+});
+
+// Insert ToDo
+app.post("/todos/add", (req, res) => {
+  console.log('Pushing Todo');
+  
+  const todo = new Todos(req.body);
+
+  todo.save()
+  .then(() => res.send({message: "Todo inserted successfully!"}))
+  .catch(e => res.send({message: e.message}))
+})
+
+// Update ToDo
+app.put("/todos/update", (req, res) => {
+  console.log('Updating Todo');
+
+  Todos.updateOne({email: req.body.email}, {age: req.body.age})
+  .then(result => res.send(result))
+  .catch(e => res.send({message: e.message}))
+})
+
+// Remove ToDo
+app.delete("/todos/remove", (req, res) => {
+  console.log('removing Todo');
+  Todos.deleteOne({title: req.body.title})
+  .then(result => res.send(result))
+  .catch(e => res.send({message: e.message}))
+})
+
+
+module.exports = app;
+
+
