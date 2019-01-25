9a626594365e1a91b7cb1829246fcda6ba87a28b
Milad Khan <mildk123@gmail.com>
2019-01-25 20:12:54 +0500

    removed from gitignore

---
+++ client/build/precache-manifest.3a118d40eff769cfb83a19e7f19571fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bafc079c924eb1eb5a4a",
+    "url": "/static/js/main.bafc079c.chunk.js"
+  },
+  {
+    "revision": "369437a77bc06c8c5898",
+    "url": "/static/js/1.369437a7.chunk.js"
+  },
+  {
+    "revision": "bafc079c924eb1eb5a4a",
+    "url": "/static/css/main.139385eb.chunk.css"
+  },
+  {
+    "revision": "b4873d37270fa997fa2f8392762094e0",
+    "url": "/index.html"
+  }
+];
